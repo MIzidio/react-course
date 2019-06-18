555a09d052a9a37b23b2342eaed59bef1b1827ed
Matheus Izidio <matheusimeira@gmail.com>
2019-06-18 15:05:35 -0300

    atualização para compnentes dinamicos

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import './App.css';
+import Person from './Person/Person';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <h1>Salve quebrada, to aprendendo react</h1>
+      <p>Ta funcionando memo em...</p>
+      <Person name="Joselito" age="30"/>
+      <Person name="Matheus" age="21">Hobbies: Mandar Salve</Person>
+      <Person name="Ralphael" age="20"/>
     </div>
   );
+  // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi I\'m a React App!!!'))
 }
 
 export default App;
