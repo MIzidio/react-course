79a2f830393ad7bb59ec260d480cbffd4f9e89ff
Matheus Izidio <matheusimeira@gmail.com>
2019-07-31 14:35:15 -0300

    prettier

--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -1,15 +1,17 @@
-import React from 'react';
+import React from "react";
 
-import './Person.css'
+import "./Person.css";
 
-const person = (props) => {
-    return (
-        <div className="Person">
-            <p onClick={props.click}>Sou {props.name} e tenho {props.age} anos!</p>
-            <p>{props.children}</p>
-            <input type="text" onChange={props.changed} value={props.name}/>
-        </div>
-    )
+const person = props => {
+  return (
+    <div className="Person">
+      <p onClick={props.click}>
+        Sou {props.name} e tenho {props.age} anos!
+      </p>
+      <p>{props.children}</p>
+      <input type="text" onChange={props.changed} value={props.name} />
+    </div>
+  );
 };
 
 export default person;
